66fd88553acf0c94bb4ca51e47e202732dabb398
Jonathan Thomassen <jt@exopi.dk>
2024-05-06 13:28:59 +0200

    New welcome message

--- PizzaPlace/Controllers/WelcomeController.cs
+++ PizzaPlace/Controllers/WelcomeController.cs
@@ -8,7 +8,7 @@ public class WelcomeController : ControllerBase
     [HttpGet]
     public IActionResult Greet()
     {
-        Console.WriteLine("Greeted guest.");
+        Console.WriteLine("Greeted guest. Woohoo!");
 
         return Ok("Welcome to this automated pizza place.");
     }
